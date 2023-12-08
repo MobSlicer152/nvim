2602955e6184baf5d8183fe2495bc97f9acae88b
MobSlicer152 <brambleclaw1414@gmail.com>
2023-12-08 11:10:04 -0500

    Initial commit

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,24 @@
+return {
+  { "iruzo/matrix-nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "matrix",
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cpp",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "vim",
+      },
+    },
+  },
+}
